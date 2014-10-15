03c77dce048f2d5c7b4ddb4932c597d02a28429b
Qiyuan Qiu <qiuqiyuan@gmail.com>
2014-10-14 23:14:31 -0400

    Initial commit

---
+++ 3.1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Please enter your first name (followed by enter):\n";
+    string first_name;
+    cin >> first_name;
+    int num_of_steps = 39;
+    string name2 = "Anne";
+    cout << "Hello, " << first_name << "!\n";
+}
---
+++ 3.7.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   //string s = "Goodbye, cruel world!";
+   string s = 7; 
+   cout << s << '\n';
+}
